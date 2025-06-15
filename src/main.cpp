c08faaf22c7e5c95ffea2f91cf9705f8c9bff9a2
JamahaW <153020210+JamahaW@users.noreply.github.com>
2025-06-15 20:42:15 +0300

    init

---
+++ src/main.cpp
@@ -0,0 +1,62 @@
+#include <Arduino.h>
+
+
+// Задача для мигания
+[[noreturn]] void blink(void *args) {
+    auto *p = static_cast<std::pair<int, int> *>(args);
+
+    const int pin = p->first, period = p->second / 2;
+
+    delete p;
+
+    pinMode(p->first, OUTPUT);
+
+    while (true) {
+        digitalWrite(pin, HIGH);
+        delay(period);
+        digitalWrite(pin, HIGH);
+        delay(period);
+    }
+}
+
+void setup() {
+    // Размер стека для задач избыточен
+    const int stack_depth = 4096;
+
+    // Создание задачи
+    xTaskCreate(
+        // Указатель на функцию, которая будет работать на задачу
+        blink,
+        // Имя задачи не обязательно, но можно использовать для отладки
+        "blink 1",
+        // Указываем глубину стека для задачи
+        stack_depth,
+        // передача параметров задачу
+        new std::pair<int, int>(4, 1000),
+        // Приоритет задачи = 0 (как у setup, loop)
+        0,
+        // Куда вернуть handler задачи (null - не передаём)
+        nullptr
+    );
+
+    // Handler второй задачи
+    TaskHandle_t blink_2 = nullptr;
+
+    // Аналогично создаём задачу
+    xTaskCreate(
+        blink,
+        nullptr,
+        stack_depth,
+        new std::pair<int, int>(15, 1000 / 3),
+        0,
+        &blink_2
+    );
+
+    // ждём 5 секунд и удаляем задачу
+    delay(5000);
+    vTaskDelete(blink_2);
+}
+
+void loop() {
+
+}

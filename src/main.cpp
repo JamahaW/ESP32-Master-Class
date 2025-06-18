ac7a065d79ea010e726505c33eb29a2b1f27e1e9
JamahaW <153020210+JamahaW@users.noreply.github.com>
2025-06-18 23:05:12 +0300

    Занятие 3 задание 2.1

--- src/main.cpp
+++ src/main.cpp
@@ -7,10 +7,7 @@ GyverOLED<SSD1306_128x64, OLED_BUFFER> oled;
 
 // Глобальная переменная, хранит последнее значение потенциометра
 volatile int pot = 0;
-
-// Флаг управления дисплеем
-volatile bool is_oled_on = true;
-
+// Квалификатор volatile гарантирует работу атомарными операциями
 
 // Задача потенциометра
 void potUpdate(void *) {
@@ -28,69 +25,47 @@ void oledUpdate(void *) {
     oled.init();
     oled.setScale(2);
 
-    bool current_power_state = true;
-
     while (true) {
-        // Проверяем, нужно ли изменить состояние питания
-        if (is_oled_on != current_power_state) {
-            oled.setPower(is_oled_on);
-            current_power_state = is_oled_on;
-        }
-
-        // Если питание включено и обновление разрешено, обновляем дисплей
-        if (is_oled_on) {
-            oled.clear();
-            oled.setCursor(0, 0);
-            oled.printf("Pot:%d", pot);
-            oled.update();
-        }
-
+        oled.clear();
+        oled.setCursor(0, 0);
+        oled.printf("Pot:%d", pot);
+        oled.update();
         delay(100); // 10 Гц
     }
-}
-
-// Задача мигания светодиодом
-void blink(void *) {
-    const int pin = 13;
-    pinMode(pin, OUTPUT);
-
-    while (true) {
-        // Вычисляем полупериод
-        int half_period = map(pot, 0, 4095, 50, 500);
-
-        digitalWrite(pin, LOW);
-        delay(half_period);
-        digitalWrite(pin, HIGH);
-        delay(half_period);
-    }
-}
-
-// Обработчик прерывания кнопки
-void IRAM_ATTR onClick() {
-    const auto debounce = 200; // длительность дребезга контактов мс
-    static auto last_click = millis(); // момент предыдущего вызова обработчика
-
-    // Антидребезг
-    if (millis() - last_click < debounce) { return; }
-    last_click = millis();
-
-    // Инвертируем состояние дисплея
-    is_oled_on = !is_oled_on;
-    Serial.printf("Click: %s\n", is_oled_on ? "On" : "Off");
 }
 
 void setup() {
     Serial.begin(115200);
 
-    // Создаем задачи
-    xTaskCreate(potUpdate, "PotUpdate", 2048, nullptr, 1, nullptr);
-    xTaskCreate(oledUpdate, "OledUpdate", 2048, nullptr, 1, nullptr);
-    xTaskCreate(blink, "Blink", 2048, nullptr, 1, nullptr);
+    // Пытаемся создать задачу для потенциометра
+    BaseType_t result = xTaskCreate(
+        // Функция, выполняемая задачей
+        potUpdate,
+        // Наименование задачи для дальнейшей отладки
+        "PotUpdate",
+        // Размер стека для этой задачи
+        2048,
+        // Параметры (Не передаём)
+        nullptr,
+        // Приоритет задачи (= 1 - Выше, чем IDLE)
+        1,
+        // Возвращаемый дескриптор (Не используем)
+        nullptr
+    );
 
-    // Настройка кнопки
-    const int pin_button = 14;
-    pinMode(pin_button, INPUT_PULLDOWN);
-    attachInterrupt(pin_button, onClick, RISING);
+    // Если не удалось создать задачи (Не хватает памяти)
+    if (pdFAIL == result) {
+        Serial.println("Не удалось создать задачу 'PotUpdate'");
+        return;
+    }
+    // Если задачу удалось создать, то она будет добавлена в очередь задач
+    // и она будет выполнена как только планировщик задач выделит процессорное время
+
+    // Аналогично, но кратко
+    if (pdFAIL == xTaskCreate(oledUpdate, "OledUpdate", 2048, nullptr, 1, nullptr)) {
+        Serial.println("Не удалось создать задачу 'OledUpdate'");
+        return;
+    }
 }
 
 void loop() {}

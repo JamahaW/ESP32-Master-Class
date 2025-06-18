77eb6f2917141497730c1e24156f0c8564471782
JamahaW <153020210+JamahaW@users.noreply.github.com>
2025-06-18 08:48:03 +0300

    Upd

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,53 @@
 /*
  * Занятие 2.
- * Задача 1
- * 1.	Подключение кнопки к ESP32 (обзор возможных вариантов).
- * Управление светодиодом с помощью тактовой кнопки INPUT, INPUT_PULLUP, INPUT_PULLDOWN.
+ * Задача 2 - Использование встроенных ёмкостных датчиков
+ * Управление светодиодом с помощью встроенной сенсорной кнопки
  */
+
+/*
+T0: GPIO 4
+T1: GPIO 0  (Boot pin - осторожно!)
+T2: GPIO 2  (Встроенный LED)
+T3: GPIO 15
+T4: GPIO 13
+T5: GPIO 12
+T6: GPIO 14
+T7: GPIO 27
+T8: GPIO 33
+T9: GPIO 32
+*/
 
 #include <Arduino.h>
 
 
-const int pin_led = 26, pin_button = 14;
+const int pin_led = 26, touch_pin = 14;
 
 void setup() {
     pinMode(pin_led, OUTPUT);
-    //                                                                              : 14 - [R 10K] - GND
-//    pinMode(pin_button, INPUT);             // Без встроенное подтяжки            : 14 - [B] - 3V3
-//    pinMode(pin_button, INPUT_PULLUP);      // Включить встроенную подтяжку в VCC : 14 - [B] - GND
-//    pinMode(pin_button, INPUT_PULLDOWN);    // Включить встроенную подтяжку в GND : 14 - [B] - 3V3
 
     Serial.begin(115200);
     Serial.println("Старт!");
 }
 
-void loop() {
-    bool state = digitalRead(pin_button);
+/*
 
-    digitalWrite(pin_led, state);
-    Serial.printf("State: %s\n", (state ? "HIGH" : "LOW"));
+ Калибровка порога
+ - Запустите код и считайте значения без касания
+ - Коснитесь сенсора и запишите новое значение
+ - Установите порог на 20-30% ниже минимального значения при касании
+
+ */
+
+void loop() {
+    // Пороговое значение
+    const int threshold = 30;
+
+    int value = touchRead(touch_pin);
+    bool touched = value < threshold;
+
+    digitalWrite(pin_led, touched);
+
+    Serial.printf("Value: %d\tTouched: %s\n", value, (touched ? "Yes" : "No"));
 
     delay(100);
 }

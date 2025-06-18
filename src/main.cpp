362aed2039d8b4690625d617547b2b31e02e14d6
JamahaW <153020210+JamahaW@users.noreply.github.com>
2025-06-18 09:08:22 +0300

    Upd

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 /*
  * Занятие 2.
- * Задача 3 - Управление яркостью светодиода с помощью ШИМ: analogWrite, ledcWrite
+ * Задача 3 - Управление яркостью светодиода с помощью ШИМ через analogWrite
  */
 
 #include <Arduino.h>
@@ -8,8 +8,15 @@
 
 const int pin_led = 26;
 
+const int pwm_resolution_bits = 10;
+
 void setup() {
     pinMode(pin_led, OUTPUT);
+
+    // Меняем частоту ШИМ
+    analogWriteFrequency(10000); // 10КГц
+    // Изменяем разрешение ШИМ
+    analogWriteResolution(pwm_resolution_bits);
 
     Serial.begin(115200);
     Serial.println("Старт!");
@@ -17,15 +24,18 @@ void setup() {
 
 void loop() {
     // Амплитуда
-    const int max_pwm = 255;
+    const int max_pwm = (1 << pwm_resolution_bits) - 1; // 2 ** pwm_resolution_bits - 1
 
-    for (int i = -max_pwm; i <= max_pwm; i++) {
+    // Длительность шага
+    const int step_ms = 1000 / max_pwm;
+
+    for (int i = -max_pwm; i < max_pwm; i++) {
         // Вычисляем уровень заполнения шим по треугольной функции: /\/\/\/\/
         int pwm = max_pwm - abs(i);
 
         analogWrite(pin_led, pwm);
 
         Serial.printf("PWM: %d\n", pwm);
-        delay(4);
+        delay(step_ms);
     }
 }

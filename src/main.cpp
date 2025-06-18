844d9d1dcb586630238d8eb88755ac6af96978cf
JamahaW <153020210+JamahaW@users.noreply.github.com>
2025-06-18 08:53:44 +0300

    Upd

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,12 @@
 /*
  * Занятие 2.
- * Задача 2 - Использование встроенных ёмкостных датчиков
- * Управление светодиодом с помощью встроенной сенсорной кнопки
+ * Задача 3 - Управление яркостью светодиода с помощью ШИМ: analogWrite, ledcWrite
  */
-
-/*
-T0: GPIO 4
-T1: GPIO 0  (Boot pin - осторожно!)
-T2: GPIO 2  (Встроенный LED)
-T3: GPIO 15
-T4: GPIO 13
-T5: GPIO 12
-T6: GPIO 14
-T7: GPIO 27
-T8: GPIO 33
-T9: GPIO 32
-*/
 
 #include <Arduino.h>
 
 
-const int pin_led = 26, touch_pin = 14;
+const int pin_led = 26;
 
 void setup() {
     pinMode(pin_led, OUTPUT);
@@ -29,25 +15,17 @@ void setup() {
     Serial.println("Старт!");
 }
 
-/*
-
- Калибровка порога
- - Запустите код и считайте значения без касания
- - Коснитесь сенсора и запишите новое значение
- - Установите порог на 20-30% ниже минимального значения при касании
-
- */
-
 void loop() {
-    // Пороговое значение
-    const int threshold = 30;
+    // Амплитуда
+    const int max_pwm = 255;
 
-    int value = touchRead(touch_pin);
-    bool touched = value < threshold;
+    for (int i = -max_pwm; i <= max_pwm; i++) {
+        // Вычисляем шим по треугольной функции
+        int pwm = max_pwm - abs(i);
 
-    digitalWrite(pin_led, touched);
+        analogWrite(pin_led, pwm);
 
-    Serial.printf("Value: %d\tTouched: %s\n", value, (touched ? "Yes" : "No"));
-
-    delay(100);
+        Serial.printf("PWM: %d\n", pwm);
+        delay(4);
+    }
 }

716a01360f449ee557f676c8583bf4f9dee78b0a
JamahaW <153020210+JamahaW@users.noreply.github.com>
2025-06-19 23:29:51 +0300

    Обновлен

--- src/main.cpp
+++ src/main.cpp
@@ -2,46 +2,14 @@
 #include <esp_now.h>
 #include <WiFi.h>
 
+void onDataReceive(const uint8_t *mac, const uint8_t *data, int size) {
+    auto message = reinterpret_cast<const char*>(data);
 
-#pragma pack(push, 1)  // Гарантированное выравнивание без паддинга
-
-struct Message {
-    char text[32];
-    int32_t counter;
-
-    void print() const {
-        Serial.printf("Message{.text=\"%s\", .counter=%d}", text, counter);
-    }
-};
-
-#pragma pack(pop)
-
-using u8 = uint8_t;
-
-void printMac(const uint8_t *mac) {
     Serial.printf(
-        "[%02X:%02X:%02X:%02X:%02X:%02X]",
-        mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]
+        "[%02X:%02X:%02X:%02X:%02X:%02X]: '%s' (%d B)\n",
+        mac[0], mac[1], mac[2], mac[3], mac[4], mac[5],
+        message, size
     );
-}
-
-void onDataReceive(const u8 *mac, const u8 *data, int size) {
-    printMac(mac);
-
-    if (size != sizeof(Message)) {
-        Serial.println(" (Неопределенный формат)");
-        for (int i = 0; i < size; i += 1) {
-            Serial.printf("%02X ", data[i]);
-            if (i % 6 == 1) { Serial.println(); }
-        }
-        Serial.println();
-
-        return;
-    }
-
-    const auto &msg = *reinterpret_cast<const Message *>(data);
-    msg.print();
-    Serial.println();
 }
 
 void setup() {
@@ -49,7 +17,7 @@ void setup() {
     WiFi.mode(WIFI_STA);
 
     if (esp_now_init() != ESP_OK) {
-        Serial.println("Не удалось инициализировать ESP-NOW");
+        Serial.println("ESP-NOW init fail");
         return;
     }
 

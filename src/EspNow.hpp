8800266f1b97f7bb5b9e7b8a7d346c0d218fdce3
JamahaW <153020210+JamahaW@users.noreply.github.com>
2025-06-20 21:19:37 +0300

    Обёртка EspNow.hpp

--- src/EspNow.hpp
+++ src/EspNow.hpp
@@ -312,7 +312,10 @@ public:
 
     static MacString toString(const Mac &mac) {
         MacString buff;
-        sprintf(buff.data(), mac_format_string, mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
+
+        auto raw = mac.data();
+        sprintf(buff.data(), mac_format_string, raw[0], raw[1], raw[2], raw[3], raw[4], raw[5]);
+
         return buff;
     }
 

4c990662c91652bca48c66ab14850c3a7256b357
JamahaW <153020210+JamahaW@users.noreply.github.com>
2025-06-21 17:12:13 +0300

    ref

--- src/main.cpp
+++ src/main.cpp
@@ -7,22 +7,22 @@
 #include <nvs_flash.h>
 #include <map>
 
-/// Логирование операций EspNow
-#define log(__EspNow_api_Result_func) ({auto __r = __EspNow_api_Result_func; Serial.printf(#__EspNow_api_Result_func " -> %s\n", EspNow::toString(__r));})
 
-/// Тип пакета сообщения от сервера
-using ServerMessage = std::array<char, 128>;
+namespace game {
+    /// Тип пакета сообщения от сервера
+    using ServerMessage = std::array<char, 128>;
 
-/// Тип пакета сообщения от игрока
-using PlayerMessage = std::array<char, 16>;
+    /// Тип пакета сообщения от игрока
+    using PlayerMessage = std::array<char, 16>;
 
-/// Тип для определения позиции
-using Position = uint8_t;
+    /// Тип для определения позиции
+    using Position = uint8_t;
 
-/// Тип пакета хода от клиента
-struct [[gnu::packed]] PlayerMove {
-    Position x, y;
-};
+    /// Тип пакета хода от клиента
+    struct [[gnu::packed]] PlayerMove {
+        Position x, y;
+    };
+}
 
 namespace game {
     /// Данные пользователя
@@ -93,8 +93,8 @@ namespace game {
                 this->onReceive(mac, data, size);
             };
 
-            log(esp_now.setDeliveryHandler(on_delivery));
-            log(esp_now.setReceiveHandler(on_receive));
+            esp_now.setDeliveryHandler(on_delivery);
+            esp_now.setReceiveHandler(on_receive);
         }
 
         void pull() {
@@ -116,7 +116,7 @@ namespace game {
                 auto record = _sends.front();
 
                 if (not EspNow::checkPeerExist(record.mac)) {
-                    log(EspNow::addPeer(record.mac));
+                    EspNow::addPeer(record.mac);
                 }
 
                 auto result = EspNow::send(record.mac, record.message);
@@ -142,12 +142,14 @@ namespace game {
             if (it == _clients.end()) {
                 // Игрок ещё не существует - регистрируем
 
-                _clients.emplace(mac, Player::create(message));
+                const auto &player = Player::create(message);
+                _clients.emplace(mac, player);
 
                 send(mac, formatted<sizeof(ServerMessage)>(
-                    "Client %s registered as '%s'",
+                    "Client %s registered as '%s' team: %d",
                     EspNow::toString(mac).data(),
-                    message.data()
+                    player.username.data(),
+                    player.team
                 ));
             } else {
                 // Игрок уже существует - изменяем имя
@@ -246,54 +248,79 @@ constexpr EspNow::Mac server = {0x78, 0x1C, 0x3C, 0xA4, 0x9E, 0x7C};
     }
 }
 
-/// Запуск клиента
-[[noreturn]] void runClient(EspNow &esp_now) {
-    // Доставка сообщений (К серверу)
-    auto on_delivery = [](const EspNow::Mac &mac, EspNow::DeliveryStatus status) {
-        Serial.printf(
-            "Delivery to server %s : %s\n",
-            EspNow::toString(mac).data(),
-            EspNow::toString(status));
-    };
+namespace game {
+    struct Client {
+        void init() {
+            auto &esp_now = EspNow::instance();
 
-    // Приём сообщений (От сервера)
-    auto on_receive = [](const EspNow::Mac &mac, const void *data, int size) {
-        // Проверка, что сообщение пришло от сервера
-        if (mac != server) {
-            Serial.printf(
-                "Message (%d Bytes) from %s (not server)\n",
-                size,
-                EspNow::toString(mac).data());
-            return;
+            auto on_delivery = [this](const EspNow::Mac &mac, EspNow::DeliveryStatus status) {
+                this->onDelivery(mac, status);
+            };
+
+            auto on_receive = [this](const EspNow::Mac &mac, const void *data, int size) {
+                this->onReceive(mac, data, size);
+            };
+
+            esp_now.setDeliveryHandler(on_delivery);
+            esp_now.setReceiveHandler(on_receive);
+
+
+            EspNow::addPeer(server);
+
+            PlayerMessage message = {"DarkWoldX17"};
+
+            EspNow::send(server, message);
         }
 
-        // Проверка размера пакета
-        if (size != sizeof(ServerMessage)) {
+        void pull() {
+            EspNow::send(server, PlayerMove{123, 69});
+        }
+
+    protected:
+
+        void onDelivery(const EspNow::Mac &mac, EspNow::DeliveryStatus status) {
             Serial.printf(
-                "Got message from %s (server) with incorrect size (%d) expected (%d)\n",
+                "Delivery to server %s : %s\n",
                 EspNow::toString(mac).data(),
-                size,
-                sizeof(ServerMessage));
-            return;
+                EspNow::toString(status)
+            );
         }
 
-        const auto &message = *static_cast<const ServerMessage *>(data);
-        Serial.print("Server: ");
-        Serial.println(message.data());
+        void onReceive(const EspNow::Mac &mac, const void *data, int size) {
+            // Проверка, что сообщение пришло от сервера
+            if (mac != server) {
+                Serial.printf(
+                    "Message (%d Bytes) from %s (not server)\n",
+                    size,
+                    EspNow::toString(mac).data());
+                return;
+            }
+
+            // Проверка размера пакета
+            if (size != sizeof(ServerMessage)) {
+                Serial.printf(
+                    "Got message from %s (server) with incorrect size (%d) expected (%d)\n",
+                    EspNow::toString(mac).data(),
+                    size,
+                    sizeof(ServerMessage));
+                return;
+            }
+
+            const auto &message = *static_cast<const ServerMessage *>(data);
+            Serial.print("Server: ");
+            Serial.println(message.data());
+        }
     };
+}
 
-    log(esp_now.setDeliveryHandler(on_delivery));
-    log(esp_now.setReceiveHandler(on_receive));
+/// Запуск клиента
+[[noreturn]] void runClient() {
+    game::Client client;
 
-    log(EspNow::addPeer(server));
-
-    PlayerMove move = {123, 69};
-    PlayerMessage message = {"DarkWoldX17"};
-
-    log(EspNow::send(server, message));
+    client.init();
 
     while (true) {
-        log(EspNow::send(server, move));
+        client.pull();
         delay(5000);
     }
 }
@@ -316,7 +343,7 @@ void setup() {
     ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
     ESP_ERROR_CHECK(esp_wifi_start());
 
-    log(EspNow::init());
+    EspNow::init();
 
     auto &esp_now = EspNow::instance();
     const bool is_server = esp_now.mac == server;
@@ -330,7 +357,7 @@ void setup() {
     if (is_server) {
         runServer();
     } else {
-        runClient(esp_now);
+        runClient();
     }
 }
 

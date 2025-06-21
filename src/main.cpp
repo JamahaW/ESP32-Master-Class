fda0cacb0227305546d667bb72daf33478cbc953
JamahaW <153020210+JamahaW@users.noreply.github.com>
2025-06-21 21:53:46 +0300

    upd

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,24 @@
-/*
- * Занятие 2.
- * Задача 4 - analogRead
- */
-
-#include <Arduino.h>
+#include "Arduino.h"
 
 
-const int pin_led = 26, pin_potentiometer = 33;
-
-const auto adc_resolution = 8; // Разрешение АЦП (1..16)
+const int PIN_JOY_X = 32;   // Аналоговый пин для оси X (ADC1)
+const int PIN_JOY_Y = 33;   // Аналоговый пин для оси Y (ADC1)
+const int PIN_JOY_BTN = 15; // Цифровой пин для кнопки (с подтяжкой)
 
 void setup() {
-    pinMode(pin_led, OUTPUT);
-
-    // Изменить разрешение АЦП
-    analogReadResolution(adc_resolution);
+    pinMode(PIN_JOY_X, INPUT);
+    pinMode(PIN_JOY_Y, INPUT);
+    pinMode(PIN_JOY_BTN, INPUT_PULLUP); // Включение внутренней подтяжки к VCC
 
     Serial.begin(115200);
-    Serial.println("Старт!");
 }
 
 void loop() {
-    int value = analogRead(pin_potentiometer);
-    analogWrite(pin_led, value);
+    int x = analogRead(PIN_JOY_X);
+    int y = analogRead(PIN_JOY_Y);
+    bool b = digitalRead(PIN_JOY_BTN); // LOW при нажатии
 
-    Serial.printf("ADC: %d\n", value);
+    Serial.printf("x: %d\ty: %d\tb: %s\n", x, y, b ? "HIGH" : "LOW");
 
-    delay(1000);
+    delay(100);
 }

8ff3c98f2d5a1575b8305a843b5e27957a6cc2fa
JamahaW <153020210+JamahaW@users.noreply.github.com>
2025-06-22 00:39:30 +0300

    upd

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include "Arduino.h"
+#include "GyverOLED.h"
 
 
 using u8 = uint8_t;
@@ -27,11 +28,10 @@ template<typename T> struct ExponentialFilter {
 };
 
 struct Joystick {
-    using Value = int;
     using TimeMs = uint32_t;
 
     struct JoystickAxis {
-        static constexpr Value max_value = 4095;
+        static constexpr int max_value = 4095;
         static constexpr auto default_center = max_value / 2;
 
         const u8 pin;
@@ -44,18 +44,18 @@ struct Joystick {
                 .pin = pin,
                 .center = default_center,
                 .center_max = default_center,
-                .filter = ExponentialFilter<float>(0.5f, 0),
+                .filter = ExponentialFilter<float>(0.3f, default_center),
             };
         }
 
-        void updateCenter(Value new_center) {
+        void updateCenter(int new_center) {
             center = float(new_center);
             center_max = max_value - center;
         }
 
         void init() const { pinMode(pin, INPUT); }
 
-        Value readRaw() const { return analogRead(pin); }
+        int readRaw() const { return analogRead(pin); }
 
         float readNormalized() {
             const auto value = filter.calc(float(readRaw()) - center);
@@ -68,6 +68,11 @@ struct Joystick {
         }
 
         JoystickAxis() = delete;
+    };
+
+    struct Value {
+        Vector2D axis;
+        float power;
     };
 
     JoystickAxis axis_x, axis_y;
@@ -87,8 +92,8 @@ struct Joystick {
     void calibrate(int iterations) {
         constexpr TimeMs period = 10;
 
-        Value sum_x = 0;
-        Value sum_y = 0;
+        int sum_x = 0;
+        int sum_y = 0;
 
         for (int i = 0; i < iterations; i++) {
             sum_x += axis_x.readRaw();
@@ -100,14 +105,16 @@ struct Joystick {
         axis_y.updateCenter(sum_y / iterations);
     }
 
-    Vector2D read() {
+    Value read() {
         const auto x = axis_x.readNormalized();
         const auto y = axis_y.readNormalized();
         const auto h = std::hypot(x, y);
 
-        if (h < 0.01) { return {0, 0}; }
+        if (h < 0.01) { return {{0, 0}, 0}; }
 
-        return {x / h, y / h};
+        if (h > 1) { return {{x / h, y / h}, 1}; }
+
+        return {{x, y}, h};
     }
 
     Joystick() = delete;
@@ -117,19 +124,58 @@ auto joystick = Joystick::create(32, 33);
 
 auto button = Button{15};
 
+auto screen = GyverOLED<SSD1306_128x64, OLED_BUFFER>();
+
 void setup() {
+    screen.init();
     joystick.init();
 
-    Serial.begin(115200);
+    Wire.setClock(1000000UL);
 
     joystick.calibrate(100);
 }
 
 void loop() {
-    const auto a = joystick.read();
+    const auto v = joystick.read();
     bool b = button.read();
 
-    Serial.printf("x: %03.2f\ty: %03.2f\tb: %s\n", a.x, a.y, b ? "HIGH" : "LOW");
+    screen.clear();
 
-    delay(100);
+    {
+        screen.setCursor(0, 0);
+        screen.printf("x %.3f", v.axis.x);
+
+        screen.setCursor(0, 1);
+        screen.printf("y %.3f", v.axis.y);
+
+        screen.setCursor(0, 2);
+        screen.printf("p %.3f", v.power);
+
+        screen.setCursor(0, 3);
+        screen.printf("b %s", b ? "high" : "low");
+    }
+
+    {
+        constexpr int
+            height = 63,
+            width = 127,
+            inner_circle_r = height / 6,
+            outer_circle_r = int(double(height) / 2),
+            pos_x = width - outer_circle_r,
+            pos_y = height / 2;
+
+        constexpr auto scale = float(outer_circle_r - inner_circle_r);
+
+        auto joy_pos_x = int(v.axis.x * scale) + pos_x;
+        auto joy_pos_y = int(v.axis.y * scale) + pos_y;
+
+        screen.circle(pos_x, pos_y, outer_circle_r, OLED_STROKE);
+        screen.circle(joy_pos_x, joy_pos_y, inner_circle_r, b ? OLED_FILL : OLED_STROKE);
+        screen.line(pos_x, pos_y, joy_pos_x, joy_pos_y);
+    }
+
+    screen.update();
+
+    const auto d = 1000 / 30;
+    delay(d);
 }

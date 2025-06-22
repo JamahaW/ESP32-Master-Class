1fca209f9dd8e543825b18e6b983ee4da5fcb90f
JamahaW <153020210+JamahaW@users.noreply.github.com>
2025-06-22 18:43:59 +0300

    Задание 2.1

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,20 @@
+// Занятие 2.1 - Базовое управление кнопкой
 #include <Arduino.h>
 
-void setup() {}
 
-void loop() {}
+// Определение пинов: светодиод и кнопка
+const auto pin_led = 26, pin_button = 14;
+
+void setup() {
+    // Настройка режимов пинов
+    pinMode(pin_led, OUTPUT);
+    pinMode(pin_button, INPUT_PULLUP);  // Используем встроенную подтяжку к VCC
+}
+
+void loop() {
+    // Чтение состояния кнопки и управление светодиодом
+    bool button_state = digitalRead(pin_button);
+    digitalWrite(pin_led, button_state);
+
+    delay(100);
+}

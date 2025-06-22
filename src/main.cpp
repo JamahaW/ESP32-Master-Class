c3227921297c71cec9122a297470b7326ec65846
JamahaW <153020210+JamahaW@users.noreply.github.com>
2025-06-22 17:56:17 +0300

    Задание 4.7

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,13 @@
-// Занятие 4.6 - Вывод сообщений Telegram на OLED
+// Занятие 4.7 - Управление LED через Telegram
 
 #include <WiFi.h>
 #include <FastBot.h>
 #include <GyverOLED.h>
 
 
-GyverOLED<SSD1306_128x64, OLED_NO_BUFFER> oled;
+const int pin_led = 2;
+
+GyverOLED<SSD1306_128x64, OLED_BUFFER> oled;
 
 auto ssid = "ИМЯ_СЕТИ", password = "ПАРОЛЬ_СЕТИ";
 
@@ -15,10 +17,15 @@ void handleMessage(FB_msg &msg) {
     oled.clear();
     oled.setCursor(0, 0);
     oled.print(msg.text);
+    oled.update();
+
+    if (msg.text == "/on") { digitalWrite(pin_led, HIGH); }
+    if (msg.text == "/off") { digitalWrite(pin_led, LOW); }
 }
 
 void setup() {
     Serial.begin(115200);
+    pinMode(pin_led, OUTPUT);
     oled.init();
     oled.autoPrintln(true);
 

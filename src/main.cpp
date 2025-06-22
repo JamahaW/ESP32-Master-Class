5e38d153a4329dc953b7a6d87fb9a95f6b509b5a
JamahaW <153020210+JamahaW@users.noreply.github.com>
2025-06-22 17:26:40 +0300

    Задание 4.1

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,18 @@
-#include <Arduino.h>
+// Занятие 4.1 - Подключение к Wi-Fi сети
+#include <WiFi.h>
 
-void setup() {}
+
+auto ssid = "ИМЯ_СЕТИ", passphrase = "ПАРОЛЬ_СЕТИ";
+
+void setup() {
+    Serial.begin(115200);
+    WiFi.begin(ssid, passphrase);
+
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+    Serial.println("\nПодключено!");
+}
 
 void loop() {}

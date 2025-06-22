b5a86d93fa2b5f2ac37eaf35c985e08201fcaa50
JamahaW <153020210+JamahaW@users.noreply.github.com>
2025-06-22 17:03:41 +0300

    Задание 3.4

--- src/main.cpp
+++ src/main.cpp
@@ -12,6 +12,9 @@ GyverOLED<SSD1306_128x64, OLED_BUFFER> oled;
 // Глобальная переменная, хранит последнее значение потенциометра
 volatile int pot = 0;
 
+// Флаг для управления дисплеем
+volatile bool oled_enabled = true;
+
 // Задача потенциометра (Обновляет pot с частотой 100 Гц)
 [[noreturn]] void potUpdate(void *) {
     const int pin_pot = 4;
@@ -29,10 +32,12 @@ volatile int pot = 0;
     oled.setScale(2);
 
     while (true) {
-        oled.clear();
-        oled.setCursor(0, 0);
-        oled.printf("Pot:%d", pot);
-        oled.update();
+        if (oled_enabled) {
+            oled.clear();
+            oled.setCursor(0, 0);
+            oled.printf("Pot:%d", pot);
+            oled.update();
+        }
         delay(100); // 10 Гц
     }
 }
@@ -53,6 +58,20 @@ volatile int pot = 0;
     }
 }
 
+// Обработчик прерывания кнопки
+void IRAM_ATTR onClick() {
+    const auto debounce = 200; // длительность дребезга контактов мс
+    static auto last_click = millis(); // момент предыдущего вызова обработчика
+
+    // Антидребезг
+    if (millis() - last_click < debounce) { return; }
+    last_click = millis();
+
+    // Инвертируем состояние дисплея
+    oled_enabled = not oled_enabled;
+    Serial.printf("Click: %s\n", oled_enabled ? "On" : "Off");
+}
+
 void setup() {
     Serial.begin(115200);
 
@@ -60,7 +79,11 @@ void setup() {
     xTaskCreate(potUpdate, "PotUpdate", 2048, nullptr, 1, nullptr);
     xTaskCreate(oledUpdate, "OledUpdate", 2048, nullptr, 1, nullptr);
     xTaskCreate(blink, nullptr, 2048, nullptr, 1, nullptr);
-    // Поскольку размер стека мал, а количество задач небольшое, в целях лаконичность обработкой ошибок можно пренебречь
+
+    // Настройка кнопки
+    const int pin_button = 14;
+    pinMode(pin_button, INPUT_PULLDOWN);
+    attachInterrupt(pin_button, onClick, RISING);
 }
 
 void loop() {}

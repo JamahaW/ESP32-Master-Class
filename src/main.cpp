596f0a171bf812e15d181b0da2b7ba3e9d1107ad
JamahaW <153020210+JamahaW@users.noreply.github.com>
2025-06-22 16:54:18 +0300

    Задание 3.1

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,69 @@
 #include <Arduino.h>
+#include <GyverOLED.h>
 
-void setup() {}
+
+GyverOLED<SSD1306_128x64, OLED_BUFFER> oled;
+
+// Глобальная переменная, хранит последнее значение потенциометра
+volatile int pot = 0;
+
+// Задача потенциометра (Обновляет pot с частотой 100 Гц)
+[[noreturn]] void potUpdate(void *) {
+    const int pin_pot = 4;
+    pinMode(pin_pot, INPUT);
+
+    while (true) {
+        pot = analogRead(pin_pot);
+        delay(10);
+    }
+}
+
+// Задача дисплея (Использует pot, обновляет дисплей с частотой 10 Гц)
+[[noreturn]] void oledUpdate(void *) {
+    oled.init();
+    oled.setScale(2);
+
+    while (true) {
+        oled.clear();
+        oled.setCursor(0, 0);
+        oled.printf("Pot:%d", pot);
+        oled.update();
+        delay(100);
+    }
+}
+
+void setup() {
+    Serial.begin(115200); // Для отладки
+
+    // Пытаемся создать задачу для потенциометра
+    BaseType_t result = xTaskCreate(
+        // Функция, выполняемая задачей
+        potUpdate,
+        // Наименование задачи для дальнейшей отладки
+        "PotUpdate",
+        // Размер стека для этой задачи
+        2048,
+        // Параметры (Не передаём)
+        nullptr,
+        // Приоритет задачи (= 1 - Выше, чем IDLE)
+        1,
+        // Возвращаемый дескриптор (Не используем)
+        nullptr
+    );
+
+    // Если не удалось создать задачи (Не хватает памяти)
+    if (pdFAIL == result) {
+        Serial.println("Не удалось создать задачу 'PotUpdate'");
+        return;
+    }
+    // Если задачу удалось создать, то она будет добавлена в очередь задач
+    // и она будет выполнена как только планировщик задач выделит процессорное время
+
+    // Аналогично, но кратко
+    if (pdFAIL == xTaskCreate(oledUpdate, "OledUpdate", 2048, nullptr, 1, nullptr)) {
+        Serial.println("Не удалось создать задачу 'OledUpdate'");
+        return;
+    }
+}
 
 void loop() {}
